9c544460a4cb67d5cb2701f797fa9e5e05e089fd
magda-coder <55542483+magda-coder@users.noreply.github.com>
2020-04-13 16:44:25 -0300

    Reajustado o projeto.

--- simple-login-project/src/App.vue
+++ simple-login-project/src/App.vue
@@ -1,10 +1,64 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link
+        v-if="authenticated"
+        to="/"
+        v-on:click.native="logout()"
+        replace
+        >Logout</router-link
+      >
+    </div>
+    <router-view @authenticated="setAuthenticated" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      authenticated: false,
+      mockAccount: {
+        username: "lol",
+        password: "123"
+      }
+    };
+  },
+  mounted() {
+    if (!this.authenticated) {
+      this.$router.replace({ name: "login" });
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
+    },
+    logout() {
+      this.authenticated = false;
+    }
+  }
+};
+</script>
+
 <style>
+body {
+  background-color: #f0f0f0;
+}
+
+h1 {
+  padding: 0;
+  margin-top: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 1024px;
+  margin: auto;
 }
 
 #nav {
@@ -21,53 +75,3 @@
 }
 </style>
 
-<template>
-    <div id="app">
-        <div id="nav">
-            <router-link v-if="authenticated" to="/" v-on:click.native="logout()" replace>Logout</router-link>
-        </div>
-        <router-view @authenticated="setAuthenticated" />
-    </div>
-</template>
-
-<script>
-    export default {
-        name: 'App',
-        data() {
-            return {
-                authenticated: false,
-                mockAccount: {
-                    username: "lol",
-                    password: "123"
-                }
-            }
-        },
-        mounted() {
-            if(!this.authenticated) {
-                this.$router.replace({ name: "login" });
-            }
-        },
-        methods: {
-            setAuthenticated(status) {
-                this.authenticated = status;
-            },
-            logout() {
-                this.authenticated = false;
-            }
-        }
-    }
-</script>
-
-<style>
-    body {
-        background-color: #F0F0F0;
-    }
-    h1 {
-        padding: 0;
-        margin-top: 0;
-    }
-    #app {
-        width: 1024px;
-        margin: auto;
-    }
-</style>
